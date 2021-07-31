d8b7c24a2938ceb14f3f8c5462dee20752065d23
Gonzalo Nahuel Vaca <vacagonzalo@gmail.com>
2021-07-31 14:43:28 -0300

    estructura de secuencia

---
+++ inc/secuencias.h
@@ -0,0 +1,8 @@
+#ifndef PROGRAMS_PDM_TP4_INC_SECUENCIAS_H_
+#define PROGRAMS_PDM_TP4_INC_SECUENCIAS_H_
+
+#include "sapi.h"
+#include "teclas.h"
+#include "leds.h"
+
+#endif 
---
+++ src/secuencias.c
@@ -0,0 +1,2 @@
+#include "secuencias.h"
+
